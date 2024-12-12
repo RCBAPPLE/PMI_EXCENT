86b5bd6ef1e46004b54727537a372ff850ca07d7
Amgoroth_19 <119618601+Amgoroth@users.noreply.github.com>
2024-12-12 18:18:41 +0100

    Add files via upload

---
+++ Questions_Logigramme/q_java.js
@@ -0,0 +1,44 @@
+// Fonction pour ouvrir le popup accès logigramme
+function openPopup_logig() {
+    document.getElementById("popup-logig").style.visibility = "visible";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup_logig() {
+    document.getElementById("popup-logig").style.visibility = "hidden";
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Fonction pour ouvrir le popup accès logigramme
+function openPopup_comm() {
+    document.getElementById("popup-comm").style.visibility = "visible";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup_comm() {
+    document.getElementById("popup-comm").style.visibility = "hidden";
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Fonction pour ouvrir le popup accès logigramme
+function openPopup_img() {
+    document.getElementById("popup-img").style.visibility = "visible";
+    document.getElementById("overlay").style.display = "block";
+}
+
+
+function closePopup_img() {
+    document.getElementById("popup-img").style.visibility = "hidden";
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Fonction pour ouvrir le popup accès logigramme
+function openPopup_info() {
+    document.getElementById("popup-info").style.visibility = "visible";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup_info() {
+    document.getElementById("popup-info").style.visibility = "hidden";
+    document.getElementById("overlay").style.display = "none";
+}
